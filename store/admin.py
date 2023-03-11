5cf9afa94325c49783eda91755860ce76764890c
Ashik Thowfeeq <ashikthowfeeq@gmail.com>
2023-03-11 12:49:55 +0530

    first commit

---
+++ store/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from store.models import Category,Products,Carts,Orders,Offers,Reviews
+
+admin.site.register(Category)
+admin.site.register(Products)
+admin.site.register(Offers)
